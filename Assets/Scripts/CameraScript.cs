a3189f35db291fbe1840f93ade85a05452b4a0ad
Александр_Мунтян <Fleksyzxc@yandex.ru>
2021-07-18 21:34:40 +0300

    first commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public PortalScript portal;
+    public PortalScript otherPortal;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookerPosition = otherPortal.transform.worldToLocalMatrix.MultiplyPoint3x4(Camera.main.transform.position);
+        lookerPosition = new Vector3(-lookerPosition.x, lookerPosition.y, -lookerPosition.z);
+
+        transform.localPosition = lookerPosition;
+
+        Quaternion difference = portal.transform.rotation * Quaternion.Inverse(otherPortal.transform.rotation * Quaternion.Euler(0, 180, 0));
+        transform.rotation = difference * Camera.main.transform.rotation;
+
+        //gameObject.GetComponent<Camera>().nearClipPlane   = lookerPosition.magnitude +1 ;
+
+        //Plane p = new Plane(-otherPortal.transform.forward, otherPortal.transform.position);
+        //Vector4 clipPlane = new Vector4(p.normal.x, p.normal.y, p.normal.z, p.distance);
+        //Vector4 clipPlaneCameraSpace = Matrix4x4.Transpose(
+        //                                   Matrix4x4.Inverse(GetComponent<Camera>().worldToCameraMatrix)) * clipPlane;
+        //var newMatrix = Camera.main.CalculateObliqueMatrix(clipPlaneCameraSpace);
+        //GetComponent<Camera>().projectionMatrix = newMatrix;
+
+    }
+}
---
+++ Assets/Scripts/PlayerMoveScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour
+{
+    public float move_Speed = 5f;
+    public float rotation_Speed = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    void FixedUpdate()
+    {
+    
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")) * move_Speed * Time.fixedDeltaTime);
+
+        transform.Rotate(0, Input.GetAxis("Mouse X") * rotation_Speed,0);
+        transform.GetChild(0).Rotate(-Input.GetAxis("Mouse Y") * rotation_Speed, 0, 0);
+        /*if (transform.GetChild(0).localRotation.eulerAngles.y != 0)
+        {
+            transform.GetChild(0).Rotate(Input.GetAxis("Mouse Y") * rotation_Speed, 0, 0);
+        }*/
+
+        if (transform.rotation.x !=0 )
+        {
+            transform.eulerAngles = new Vector3(0, transform.rotation.y, 0);
+        }
+        
+    }
+}
